5403a33bfc34cd29edfdef95673ad3b7259e1f2f
Ilya <dnamix1@gmail.com>
2021-02-07 12:26:19 +0300

    added mobile bottom navbar

--- client/src/components/MobileNavbar.js
+++ client/src/components/MobileNavbar.js
@@ -1,26 +1,34 @@
-import React from "react";
-import Wrapper from "../styles/MobileNavbar";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Wrapper from '../styles/MobileNavbar';
 import {
   HistoryIcon,
   HomeIcon,
   SubIcon,
   TrendingIcon,
   WatchIcon,
-} from "./Icons";
+} from './Icons';
 
 function MobileNavbar() {
   return (
     <Wrapper>
-      <div className="icons">
-        <HomeIcon />
+      <div className='icons'>
+        <NavLink exact to='/' activeClassName='active'>
+          <HomeIcon />
+        </NavLink>
+        <NavLink to='/feed/trending' activeClassName='active'>
+          <TrendingIcon />
+        </NavLink>
+        <NavLink to='/feed/subscriptions' activeClassName='active'>
+          <SubIcon />
+        </NavLink>
 
-        <TrendingIcon />
-
-        <SubIcon />
-
-        <HistoryIcon />
-
-        <WatchIcon />
+        <NavLink to='/feed/history' activeClassName='active'>
+          <HistoryIcon />
+        </NavLink>
+        <NavLink to='/feed/liked_videos' activeClassName='active'>
+          <WatchIcon />
+        </NavLink>
       </div>
     </Wrapper>
   );
