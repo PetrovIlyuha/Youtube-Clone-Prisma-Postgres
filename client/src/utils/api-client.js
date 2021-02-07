19a17178b46d42388175625a16db2a8c13e3170a
Ilya <dnamix1@gmail.com>
2021-02-07 12:28:20 +0300

    api-client functional requests functions for comments, video views, subscriptions and likes

--- client/src/utils/api-client.js
+++ client/src/utils/api-client.js
@@ -1,20 +1,68 @@
-export function authenticate() {}
+import axios from 'axios';
+import { queryCache } from 'react-query';
 
-export async function signoutUser() {}
+export const client = axios.create({
+  baseURL: '/api/v1',
+});
+
+export function authenticate(response) {
+  client({
+    method: 'POST',
+    url: '/auth/google-login',
+    data: {
+      idToken: response.tokenId,
+    },
+  })
+    .then(res => {
+      console.log(`Signin success: `, res);
+      window.location.assign(window.location.href);
+    })
+    .catch(error => {
+      console.log(`Sign in error: ${error.response}`);
+    });
+}
+
+export async function signoutUser() {
+  await client.get('/auth/signout');
+  window.location.pathname = '/';
+}
 
 export async function updateUser() {}
 
-export async function addVideoView() {}
+export async function addVideoView(videoId) {
+  await client.get(`/videos/${videoId}/view`);
+  await queryCache.invalidateQueries('History');
+}
 
-export async function addComment() {}
+export async function addComment({ video, comment }) {
+  await client.post(`/videos/${video.id}/comments`, { text: comment });
+  await queryCache.invalidateQueries(['queryVideoById', video.id]);
+}
 
-export async function addVideo() {}
+export async function addVideo(video) {
+  await client.post(`/videos`, video);
+  await queryCache.invalidateQueries('Channel');
+}
 
-export async function toggleSubscribeUser() {}
+export async function toggleSubscribeUser(channelId) {
+  await client.get(`/users/${channelId}/toggle-subscribe`);
+  await queryCache.invalidateQueries('Channel');
+  await queryCache.invalidateQueries('Channels');
+  await queryCache.invalidateQueries('Subscriptions');
+  await queryCache.invalidateQueries('AuthProvider');
+  await queryCache.invalidateQueries('queryVideoById');
+  await queryCache.invalidateQueries('SearchResults');
+}
 
-export async function likeVideo() {}
+export async function likeVideo(videoId) {
+  await client.get(`/videos/${videoId}/like`);
+  await queryCache.invalidateQueries(['queryVideoById', videoId]);
+}
 
-export async function dislikeVideo() {}
+export async function dislikeVideo(videoId) {
+  await client.get(`/videos/${videoId}/dislike`);
+  await queryCache.invalidateQueries(['queryVideoById', videoId]);
+}
 
 export async function deleteVideo() {}
 
