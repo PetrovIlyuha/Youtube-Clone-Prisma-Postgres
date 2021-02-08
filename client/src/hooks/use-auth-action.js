09d808999c40ad6f1bba3c653e382a0d45939eab
Ilya <dnamix1@gmail.com>
2021-02-08 15:42:24 +0300

    added custom hooks for detection of window.location change to close sidebar and compulsory user login on action that demands it (like, attempt to subscribe etc.

--- client/src/hooks/use-auth-action.js
+++ client/src/hooks/use-auth-action.js
@@ -1,1 +1,27 @@
-export default function useAuthAction() {}
+import { useAuth } from 'context/auth-context';
+import { useGoogleLogin } from 'react-google-login';
+import { useSnackbar } from 'react-simple-snackbar';
+import { authenticate } from 'utils/api-client';
+
+export default function useAuthAction() {
+  const user = useAuth();
+  const [openSnackbar] = useSnackbar();
+  const { signIn } = useGoogleLogin({
+    onSuccess: authenticate,
+    clientId:
+      '361987034059-u80jcnufuuftjv4i29ri37mi2mgeoab0.apps.googleusercontent.com',
+  });
+
+  function handleAuthAction(authAction, data) {
+    if (user) {
+      authAction(data);
+    } else {
+      openSnackbar('AutoMAGICally logging you in with Google!');
+      setTimeout(async () => {
+        await signIn();
+        authAction(data);
+      }, 500);
+    }
+  }
+  return handleAuthAction;
+}
--- client/src/hooks/use-location-change.js
+++ client/src/hooks/use-location-change.js
@@ -1,3 +1,20 @@
-function usePrevious() {}
+import { useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
 
-export function useLocationChange() {}
+function usePrevious(value) {
+  const locationRef = useRef(null);
+  useEffect(() => {
+    locationRef.current = value;
+  }, [value]);
+  return locationRef.current;
+}
+
+export function useLocationChange(callback) {
+  const location = useLocation();
+  const prevLocation = usePrevious(location);
+  useEffect(() => {
+    if (prevLocation?.pathname !== location.pathname) {
+      callback();
+    }
+  }, [location, prevLocation, callback]);
+}
