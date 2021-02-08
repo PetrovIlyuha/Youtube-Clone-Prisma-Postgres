eaab630438e4463948a764e460dd1a7dbdbca5b6
Ilya <dnamix1@gmail.com>
2021-02-08 15:40:03 +0300

    navbar rendering dependent on user authentication state

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,37 +1,40 @@
-import React from "react";
-import Wrapper from "../styles/Navbar";
-import GoogleAuth from "./GoogleAuth";
-import { AppsIcon, HamburgerIcon, LogoIcon, SettingsIcon } from "./Icons";
-import Search from "./Search";
+import { useAuth } from 'context/auth-context.jsx';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Wrapper from '../styles/Navbar';
+import GoogleAuth from './GoogleAuth';
+import { AppsIcon, HamburgerIcon, LogoIcon, SettingsIcon } from './Icons';
+import Search from './Search';
+import UploadVideo from './UploadVideo';
+import UserDropdown from './UserDropdown';
 
-function Navbar() {
+function Navbar({ toggleSideBarOpen }) {
+  const user = useAuth();
   return (
     <Wrapper>
-      <div className="logo flex-row">
-        <HamburgerIcon className="toggle-navhandler" />
+      <div className='logo flex-row'>
+        <HamburgerIcon
+          className='toggle-navhandler'
+          onClick={toggleSideBarOpen}
+        />
         <span>
-          <LogoIcon
-            style={{
-              width: 80,
-              height: 24,
-            }}
-          />
+          <NavLink to='/'>
+            <LogoIcon
+              style={{
+                width: 80,
+                height: 24,
+              }}
+            />
+          </NavLink>
         </span>
       </div>
 
       <Search />
 
       <ul>
-        <li>
-          <AppsIcon />
-        </li>
-        <li>
-          <SettingsIcon />
-        </li>
-        <li>
-          {" "}
-          <GoogleAuth />
-        </li>
+        <li>{user ? <UploadVideo /> : <AppsIcon />}</li>
+        <li>{user ? <AppsIcon /> : <SettingsIcon />}</li>
+        <li> {user ? <UserDropdown user={user} /> : <GoogleAuth />}</li>
       </ul>
     </Wrapper>
   );
