06fea33b411f475a5576ce1bed4da749d226b705
Ilya <dnamix1@gmail.com>
2021-02-08 15:43:27 +0300

    google login authentication chain component

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,17 +1,31 @@
-import React from "react";
-import Button from "../styles/Auth";
-import { SignInIcon } from "./Icons";
+import React from 'react';
+import Button from '../styles/Auth';
+import { GoogleLogin } from 'react-google-login';
+import { SignInIcon } from './Icons';
+import { authenticate } from 'utils/api-client';
 
 function GoogleAuth() {
   return (
-    <Button tabIndex={0} type="button">
-      <span className="outer">
-        <span className="inner">
-          <SignInIcon />
-        </span>
-        sign in
-      </span>
-    </Button>
+    <GoogleLogin
+      clientId='361987034059-u80jcnufuuftjv4i29ri37mi2mgeoab0.apps.googleusercontent.com'
+      cookiePolicy='single_host_origin'
+      onSuccess={authenticate}
+      onFailure={authenticate}
+      render={renderProps => (
+        <Button
+          tabIndex={0}
+          type='button'
+          onClick={renderProps.onClick}
+          disabled={renderProps.disabled}>
+          <span className='outer'>
+            <span className='inner'>
+              <SignInIcon />
+            </span>
+            sign in
+          </span>
+        </Button>
+      )}
+    />
   );
 }
 
