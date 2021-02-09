b86200c97c775e5caaab91597491e051448cc406
Ilya <dnamix1@gmail.com>
2021-02-09 17:50:54 +0300

    video player component

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -1,13 +1,36 @@
-import React from "react";
-import "video.js/dist/video-js.css";
+import React, { useEffect, useRef } from 'react';
+import { addVideoView } from 'utils/api-client';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
 
-function VideoPlayer() {
+function VideoPlayer({ previewUrl, video }) {
+  const videoRef = useRef(null);
+
+  const { id, url, thumbnail } = video;
+  useEffect(() => {
+    const vjsplayer = videojs(videoRef.current);
+    if (!previewUrl) {
+      vjsplayer.poster(thumbnail);
+      vjsplayer.src(url);
+    }
+    if (previewUrl) {
+      vjsplayer.src({ type: 'video/mp4', src: previewUrl });
+    }
+    vjsplayer.on('ended', () => {
+      addVideoView(id);
+    });
+    return () => {
+      if (vjsplayer) {
+        vjsplayer.dispose();
+      }
+    };
+  }, [previewUrl, thumbnail, url, id]);
   return (
     <div data-vjs-player>
       <video
         controls
-        className="video-js vjs-fluid vjs-big-play-centered"
-      ></video>
+        ref={videoRef}
+        className='video-js vjs-fluid vjs-big-play-centered'></video>
     </div>
   );
 }
