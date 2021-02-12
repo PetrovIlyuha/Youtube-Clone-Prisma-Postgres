ffdfcb5a286015ef14d4f885e2247588afba448f
Ilya <dnamix1@gmail.com>
2021-02-12 17:58:50 +0300

    single video channel component

--- client/src/components/ChannelInfo.js
+++ client/src/components/ChannelInfo.js
@@ -1,28 +1,48 @@
 // @ts-nocheck
-import React from "react";
-import Button from "../styles/Button";
-import Wrapper from "../styles/ChannelInfo";
+import useAuthAction from 'hooks/use-auth-action';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { toggleSubscribeUser } from 'utils/api-client';
+import Button from '../styles/Button';
+import Wrapper from '../styles/ChannelInfo';
 
-function ChannelInfo() {
+function ChannelInfo({ channel }) {
+  const handleAuthAction = useAuthAction();
+
+  const handleToggleSubscribe = () => {
+    handleAuthAction(toggleSubscribeUser, channel.id);
+  };
+
   return (
     <Wrapper>
-      <span className="avatar-channel">
-        <img src="https://dummyimage.com/100x100" alt="avatar" />
+      <Link className='avatar-channel' to={`/channel/${channel.id}`}>
+        <img src={channel.avatar} alt='avatar' />
 
-        <div className="channel-info-meta">
-          <h3>Username</h3>
+        <div className='channel-info-meta'>
+          <h3>{channel.username}</h3>
 
-          <p className="secondary">
-            <span>Subscribers Count subscribers</span>{" "}
-            <span className="to-hide">•</span>{" "}
-            <span className="to-hide">Videos Count videos</span>
+          <p className='secondary'>
+            <span>{channel.subscribersCount} subscribers</span>{' '}
+            <span className='to-hide'>•</span>{' '}
+            <span className='to-hide'>{channel.videoCount} videos</span>
           </p>
 
-          <p className="description secondary">Channel Description</p>
+          <p className='description secondary'>
+            {channel.about.length > 65
+              ? channel.about.slice(0, 65)
+              : channel.about}
+          </p>
         </div>
-      </span>
+      </Link>
 
-      <Button>Subscribe</Button>
+      {!channel.isMe && !channel.isSubscribed && (
+        <Button onClick={handleToggleSubscribe}>Subscribe</Button>
+      )}
+      {!channel.isMe && channel.isSubscribed && (
+        <Button grey onClick={handleToggleSubscribe}>
+          Subscribed
+        </Button>
+      )}
     </Wrapper>
   );
 }
