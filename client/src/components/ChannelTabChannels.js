6de46a7e6af566d699bcfcaa4c385857e54e037b
Ilya <dnamix1@gmail.com>
2021-02-12 18:02:53 +0300

    channel tabs added

--- client/src/components/ChannelTabChannels.js
+++ client/src/components/ChannelTabChannels.js
@@ -1,8 +1,31 @@
-import React from "react";
-import Wrapper from "../styles/ChannelTabChannels";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Wrapper from '../styles/ChannelTabChannels';
 
-function ChannelTabChannels() {
-  return <Wrapper>Channel Channels</Wrapper>;
+function ChannelTabChannels({ channels }) {
+  if (!channels.length) {
+    return <p>Not subscribed to any channels yet.</p>;
+  }
+  return (
+    <Wrapper>
+      {channels.length
+        ? channels.map(channel => (
+            <Link to={`/channel/${channel.id}`} key={channel.id}>
+              <div className='channel'>
+                <img
+                  src={channel.avatar}
+                  alt={`${channel.username}'s channel avatar`}
+                />
+                <h3>{channel.username}</h3>
+                <p className='secondary'>
+                  {channel.subscribersCount} subscribers
+                </p>
+              </div>
+            </Link>
+          ))
+        : null}
+    </Wrapper>
+  );
 }
 
 export default ChannelTabChannels;
--- client/src/components/ChannelTabVideo.js
+++ client/src/components/ChannelTabVideo.js
@@ -1,8 +1,28 @@
-import React from "react";
-import Wrapper from "../styles/ChannelTabVideo";
+import React from 'react';
+import Wrapper from '../styles/ChannelTabVideo';
+import VideoCard from './VideoCard';
 
-function ChannelTabVideo() {
-  return <Wrapper>Channel Videos</Wrapper>;
+function ChannelTabVideo({ videos }) {
+  if (videos.length === 0) {
+    return <p>This channel hasn't posted any videos yet</p>;
+  }
+  return (
+    <Wrapper>
+      <div className='videos'>
+        {videos.map(video => (
+          <div style={{ margin: '2rem 0' }}>
+            <VideoCard
+              style={{ minHeight: '12rem' }}
+              key={video.id}
+              noUsername
+              hideAvatar
+              video={video}
+            />
+          </div>
+        ))}
+      </div>
+    </Wrapper>
+  );
 }
 
 export default ChannelTabVideo;
