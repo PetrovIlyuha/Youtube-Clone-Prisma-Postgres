629b323e40b9889690f7299a5f5bfaa241c9dd6d
Ilya <dnamix1@gmail.com>
2021-02-12 18:00:17 +0300

    subscriptions component

--- client/src/pages/Subscriptions.js
+++ client/src/pages/Subscriptions.js
@@ -1,28 +1,58 @@
 // @ts-nocheck
-import React from "react";
-import { SubIcon } from "../components/Icons";
-import SignUpCard from "../components/SignUpCard";
-import Wrapper from "../styles/Home";
-import VideoGrid from "../styles/VideoGrid";
+import React from 'react';
+import ChannelSuggestions from 'components/ChannelSuggestions';
+import HomeSkeleton from 'skeletons/HomeSkeleton';
+import { SubIcon } from '../components/Icons';
+import SignUpCard from '../components/SignUpCard';
+import { useAuth } from 'context/auth-context';
+import VideoGrid from '../styles/VideoGrid';
+import Wrapper from '../styles/Home';
+import { useQuery } from 'react-query';
+import { client } from 'utils/api-client';
+import ErrorMessage from 'components/ErrorMessage';
+import VideoCard from 'components/VideoCard';
 
 function Subscriptions() {
-  const isAuth = false;
+  const user = useAuth();
 
-  if (!isAuth) {
+  const {
+    data: feed,
+    isLoading,
+    isError,
+    error,
+    isSuccess,
+  } = useQuery(
+    'Subscriptions',
+    () => client.get('/users/subscriptions').then(res => res.data.feed),
+    { enabled: user },
+  );
+  if (!user) {
     return (
       <SignUpCard
         icon={<SubIcon />}
         title="Don't miss new videos"
-        description="Sign in to see updates from your favorite YouTube channels"
+        description='Sign in to see updates from your favorite YouTube channels'
       />
     );
   }
 
+  if (isLoading) return <HomeSkeleton />;
+  if (isError) return <ErrorMessage error={error} />;
+  if (!isLoading && !feed.length) {
+    return <ChannelSuggestions />;
+  }
+
   return (
     <Wrapper>
-      <div style={{ marginTop: "1.5rem" }}></div>
+      <div style={{ marginTop: '1.5rem' }}></div>
 
-      <VideoGrid>Subscription Videos</VideoGrid>
+      <VideoGrid>
+        {isSuccess
+          ? feed.map(video => (
+              <VideoCard key={video.id} video={video} hideAvatar />
+            ))
+          : null}
+      </VideoGrid>
     </Wrapper>
   );
 }
