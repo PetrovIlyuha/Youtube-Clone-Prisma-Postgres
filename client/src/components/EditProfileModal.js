7cbf603655d0f3dc48cdfb53d83ec6bf4f43e303
Ilya <dnamix1@gmail.com>
2021-02-13 14:26:49 +0300

    edit profile and it's modal components

--- client/src/components/EditProfileModal.js
+++ client/src/components/EditProfileModal.js
@@ -1,62 +1,109 @@
-import React from "react";
-import Button from "../styles/Button";
-import Wrapper from "../styles/EditProfileModal";
-import { CloseIcon } from "./Icons";
+import React, { useState } from 'react';
+import { useSnackbar } from 'react-simple-snackbar';
+import { updateUser } from 'utils/api-client';
+import { uploadMedia } from 'utils/upload-media';
+import Button from '../styles/Button';
+import Wrapper from '../styles/EditProfileModal';
+import { CloseIcon } from './Icons';
 
-function EditProfileModal() {
+function EditProfileModal({ profile, closeModal }) {
+  const [cover, setCover] = useState(profile.cover);
+  const [avatar, setAvatar] = useState(profile.avatar);
+  const [openSnakbar] = useSnackbar();
+  const handleCoverUpload = async e => {
+    const file = e.target.files[0];
+    if (file) {
+      const cover = await uploadMedia({
+        type: 'image',
+        file,
+        preset: 'znjb2esm',
+      });
+      setCover(cover);
+    }
+  };
+
+  const handleAvatarUpload = async e => {
+    const file = e.target.files[0];
+    if (file) {
+      const avatar = await uploadMedia({
+        type: 'image',
+        file,
+        preset: 'ildby9ir',
+      });
+      setAvatar(avatar);
+    }
+  };
+
+  const handleEditProfile = async e => {
+    e.preventDefault();
+    const username = e.target.elements.username.value;
+    const about = e.target.elements.about.value;
+    if (!username.trim()) {
+      return openSnakbar('Username should not be left out empty');
+    }
+    const user = { username, about, avatar, cover };
+    await updateUser(user);
+    openSnakbar('Your profile is now updated!');
+    setTimeout(() => {
+      closeModal();
+    }, 500);
+  };
   return (
     <Wrapper>
-      <div className="container"></div>
-      <div className="edit-profile">
-        <form>
-          <div className="modal-header">
+      <div className='container'></div>
+      <div className='edit-profile'>
+        <form onSubmit={handleEditProfile}>
+          <div className='modal-header'>
             <h3>
-              <CloseIcon />
+              <CloseIcon onClick={closeModal} />
               <span>Edit Profile</span>
             </h3>
-            <Button type="submit">Save</Button>
+            <Button type='submit'>Save</Button>
           </div>
 
-          <div className="cover-upload-container">
-            <label htmlFor="cover-upload">
+          <div className='cover-upload-container'>
+            <label htmlFor='cover-upload'>
               <img
-                className="pointer"
-                width="100%"
-                height="200px"
-                src="https://dummyimage.com/600x200"
-                alt="cover"
+                className='pointer'
+                width='100%'
+                height='200px'
+                src={cover}
+                alt='cover'
               />
             </label>
             <input
-              id="cover-upload"
-              type="file"
-              accept="image/*"
-              style={{ display: "none" }}
+              id='cover-upload'
+              type='file'
+              accept='image/*'
+              style={{ display: 'none' }}
+              onChange={handleCoverUpload}
             />
           </div>
 
-          <div className="avatar-upload-icon">
-            <label htmlFor="avatar-upload">
-              <img
-                src="https://dummyimage.com/250x250"
-                className="pointer avatar lg"
-                alt="avatar"
-              />
+          <div className='avatar-upload-icon'>
+            <label htmlFor='avatar-upload'>
+              <img src={avatar} className='pointer avatar lg' alt='avatar' />
             </label>
             <input
-              id="avatar-upload"
-              type="file"
-              accept="image/*"
-              style={{ display: "none" }}
+              id='avatar-upload'
+              type='file'
+              accept='image/*'
+              onChange={handleAvatarUpload}
+              style={{ display: 'none' }}
             />
           </div>
           <input
-            type="text"
-            placeholder="Insert username"
-            id="username"
+            type='text'
+            placeholder='Insert username'
+            id='username'
+            defaultValue={profile.username}
             required
           />
-          <textarea id="about" placeholder="Tell viewers about your channel" />
+          <textarea
+            id='about'
+            defaultValue={profile.about}
+            placeholder='Tell viewers about your channel'
+          />
         </form>
       </div>
     </Wrapper>
