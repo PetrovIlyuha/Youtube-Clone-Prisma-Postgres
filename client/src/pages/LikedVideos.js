60a6ace29bc9ce561b334a1cbda2603bf249f32e
Ilya <dnamix1@gmail.com>
2021-02-15 17:40:12 +0300

    liked videos feed

--- client/src/pages/LikedVideos.js
+++ client/src/pages/LikedVideos.js
@@ -1,26 +1,55 @@
-import React from "react";
-import { ChannelIcon } from "../components/Icons";
-import SignUpCard from "../components/SignUpCard";
-import Wrapper from "../styles/Trending";
+import ErrorMessage from 'components/ErrorMessage';
+import TrendingCard from 'components/TrendingCard';
+import { useAuth } from 'context/auth-context';
+import React from 'react';
+import { useQuery } from 'react-query';
+import { Link } from 'react-router-dom';
+import TrendingSkeleton from 'skeletons/TrendingSkeleton';
+import { client } from 'utils/api-client';
+import { ChannelIcon } from '../components/Icons';
+import SignUpCard from '../components/SignUpCard';
+import Wrapper from '../styles/Trending';
 
 function LikedVideos() {
-  const isAuth = false;
+  const user = useAuth();
 
-  if (!isAuth) {
+  const {
+    data: videos,
+    isLoading,
+    isError,
+    isSuccess,
+    error,
+  } = useQuery(
+    'LikedVideos',
+    () => client.get('/users/liked-videos').then(res => res.data.videos),
+    { enabled: true },
+  );
+
+  if (!user) {
     return (
       <SignUpCard
         icon={<ChannelIcon />}
-        title="Save everything you like"
-        description="Videos that you have liked will show up here"
+        title='Save everything you like'
+        description='Videos that you have liked will show up here'
       />
     );
   }
+  if (isLoading) return <TrendingSkeleton />;
+  if (isError) return <ErrorMessage error={error} />;
 
   return (
     <Wrapper>
       <h2>Liked Videos</h2>
-      <p className="secondary">Videos that you have liked will show up here</p>
-      Liked Videos
+      {isSuccess && !videos.length && (
+        <div className='secondary'>Liked videos will be appearing here</div>
+      )}
+      {isSuccess &&
+        videos.length &&
+        videos.map(video => (
+          <Link key={video.id} to={`/watch/${video.id}`}>
+            <TrendingCard video={video} />
+          </Link>
+        ))}
     </Wrapper>
   );
 }
